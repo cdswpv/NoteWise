225e74776e8cb4402ed1e73f90b40d7e00280613
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-09-27 14:34:07 -0600

    added in a scrpting permission to allow us to grab text off a webpage, as of now i am just grabing the fist para that the queryselector finds.

--- content.js
+++ content.js
@@ -1,9 +1,51 @@
 document.getElementById("myButton").addEventListener("click", getTab);
 
-function getTab(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var tab = tabs[0];
-        console.log(tab.url);
-        document.getElementById("demo").innerHTML = tab.url;
-      });
+function getTab() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tab = tabs[0];
+    console.log(tab.url);
+
+
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        function: getPageText,
+      },
+      function (result) {
+        const pageText = result[0].result;
+
+        
+        const test = getSentences(pageText);
+        document.getElementById("demo").textContent = test;
+      }
+    );
+  });
 }
+
+function getPageText() {
+    const mainContent = ["p", "div", "article"];
+
+    for (const selector of mainContent){
+      const element = document.querySelector(selector);
+
+      if(element){
+        return element.innerText; 
+      }
+    }
+    return " ";
+}
+
+function getSentences(pageText){
+
+  const sentences = pageText.split(/[.!?]/);
+
+  const validSentences = sentences.filter(sentences => sentences.trim().length > 1);
+
+  const testSentences = validSentences.slice(0,5);
+
+  return testSentences.join('. ') + '.';
+
+}
+
+
