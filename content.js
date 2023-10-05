3016ebb04503f02444062f5d3307a963ce9b757d
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-10-05 13:59:22 -0500

    Broke in to paragraphs and displayed on screen

--- content.js
+++ content.js
@@ -4,21 +4,51 @@ function getText() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var tab = tabs[0];
     console.log(tab.url);
-  
-      fetch(tab.url)
-      .then(Response => Response.text())
+
+    fetch(tab.url)
+      .then(response => response.text())
       .then(html => {
-        parseHTML(html);
-      })
-  
-    function parseHTML(html){
+        const concatenatedText = parseHTML(html);
+        const paragraphs = createPara(concatenatedText);
+        // Update the "demo" element
+        document.getElementById("demo").textContent = paragraphs.join("\n\n"); // Join paragraphs with double line breaks
+      });
+
+    function parseHTML(html) {
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, 'text/html');
-    
-      const paragraphs = doc.querySelectorAll('p','td');
+      const paragraphs = doc.querySelectorAll('p, td, ul, li');
+      let concatenatedText = '';
       paragraphs.forEach(paragraph => {
-        console.log(paragraph.textContent);
-      })
+        concatenatedText += paragraph.textContent + ' '; // Add a space between paragraphs
+      });
+      return concatenatedText;
+    }
+
+    function createPara(concatenatedText) {
+      const sentences = concatenatedText.split(/[.!?]/);
+
+      const paragraphs = [];
+
+      let currentParagraph = '';
+
+      for (let i = 0; i < sentences.length; i++) {
+        const sentence = sentences[i].trim();
+
+        if (currentParagraph.split('.').length < 6) {
+          currentParagraph += sentence + '.';
+        } else {
+          paragraphs.push(currentParagraph);
+
+          currentParagraph = sentence + ' ';
+        }
       }
-  })
+
+      if (currentParagraph.trim() !== '') {
+        paragraphs.push(currentParagraph);
+      }
+
+      return paragraphs;
+    }
+  });
 }
