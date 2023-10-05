2e60df39f28cf1c2bcafcee4cc33cb194b7db751
Juicemankam5 <kamryngrant15@gmail.com>
2023-10-05 14:04:30 -0500

    Added X Button

--- content.js
+++ content.js
@@ -1,24 +1,32 @@
 document.getElementById("myButton").addEventListener("click", getText);
+document.getElementById("x").addEventListener("click", closeElement);
 
 function getText() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var tab = tabs[0];
     console.log(tab.url);
-  
-      fetch(tab.url)
-      .then(Response => Response.text())
-      .then(html => {
-        parseHTML(html);
-      })
-  
-    function parseHTML(html){
+
+    fetch(tab.url)
+        .then(response => response.text())
+        .then(html => {
+          parseHTML(html);
+        });
+
+    function parseHTML(html) {
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, 'text/html');
-    
-      const paragraphs = doc.querySelectorAll('p','td');
+
+      const paragraphs = doc.querySelectorAll('p', 'td');
       paragraphs.forEach(paragraph => {
         console.log(paragraph.textContent);
-      })
-      }
-  })
+      });
+    }
+  });
 }
+
+function closeElement() {
+  const element = document.getElementById('x');
+  element.style.display = 'none';
+  window.close();
+}
+
