0cf6b561ee6d6b8fc57ed53b037272277fb31c8f
Colton Sharp <coltonksharp@gmail.com>
2023-11-02 14:26:31 -0500

    Updated File

--- background.js
+++ background.js
@@ -1,42 +1,62 @@
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (message.type === "text") {
-    const paragraphArray = message.textContent;
-    
-    try {
-      const response = await fetch(chrome.runtime.getURL('config.json'));
-      const config = await response.json();
-      const apiKey = config.apiKey;
 
-      const apiResponse = await fetch('https://api.openai.com/v1/engines/gpt-3.5-turbo/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify({
-          prompt: paragraphArray.join("\n"),
-          max_tokens: 50, 
-        }),
-      });
 
-      if (!apiResponse.ok) {
-        throw new Error(`HTTP error! Status: ${apiResponse.status}`);
-      }
 
-      const apiData = await apiResponse.json();
-      const apiSummary = apiData.choices[0].text;
 
-      // Send the API summary back to content.js
-      chrome.runtime.sendMessage({ type: "summary", summary: apiSummary });
-    } catch (error) {
-      console.log('Error:', error);
-      const errorMessage = 'Error occurred during summarization.';
-      
-      // Send an error message back to content.js
-      chrome.runtime.sendMessage({ type: "summary", summary: errorMessage });
-    }
-  }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const login_API_KEY = "809053939553-6854kil5qm47qqc99a268u63hbcov074.apps.googleusercontent.com";
 let user_sign_in = false;
